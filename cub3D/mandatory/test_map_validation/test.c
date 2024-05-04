c110f8afd0064550417ee292352b755c7abe5031
brunofmsilva11 <brunofmsilva.bs@gmail.com>
2024-05-04 02:09:10 +0100

    validar mapa OK

--- cub3D/mandatory/test_map_validation/test.c
+++ cub3D/mandatory/test_map_validation/test.c
@@ -165,7 +165,7 @@ int is_map_closed2(t_data *d)
 	return (1);
 }
 
-int	is_map_closed(t_data *d)
+/* int	is_map_closed(t_data *d)
 {
 	int i;
 	int j;
@@ -213,9 +213,157 @@ int	is_map_closed(t_data *d)
 		i++;
 	}
 	return (1);
+} */
+
+void skip_lines(t_data *d, int count)
+{
+    char *line;
+    int i;
+
+	line = NULL;
+	i = count;
+    while (i != 0)
+	{
+        line = get_next_line(d->fd);
+        if (!line)
+            break;
+        i--;
+        free(line);
+    }
 }
 
-void	map_validation_test(t_data *d, char *file_name)
+void read_map_lines(t_data *d, int *lines_read)
+{
+    char *line;
+
+	line = NULL;
+    while (1)
+	{
+        line = get_next_line(d->fd);
+        if (!line)
+            break;
+        if (d->line_length < (int)ft_strlen(line))
+            d->line_length = ft_strlen(line) - 1;
+        if (ft_strcmp(line, "\n"))
+		{
+            printf("%s", line);
+            (*lines_read)++;
+        }
+        free(line);
+    }
+}
+
+/* char	*read_map_lines2(t_data *d, char *file_name)
+{
+    char *line;
+
+	line = NULL;
+    close(d->fd);
+    d->fd = open(file_name, O_RDONLY);
+    skip_lines(d, 6);
+    line = get_next_line(d->fd);
+    return (line);
+}*/
+
+void	process_map_line(t_data *d, char *line)
+{
+	int i;
+	int j;
+
+	i = d->map_utils->skip_count;
+	j = 0;
+    if (check_char(line))
+	{
+		d->map_utils->map[i] = malloc(sizeof(char) * (d->line_length + 2));
+		while (j < d->line_length)
+		{
+			while (line[j] != '\0' && line[j] != '\n')
+			{
+				d->map_utils->map[i][j] = line[j];
+				j++;
+			}
+			while (j < d->line_length)
+			{
+				d->map_utils->map[i][j] = ' ';
+				j++;
+			}
+		}
+		d->map_utils->map[i][j] = '\n';
+		d->map_utils->map[i][j + 1] = '\0';
+	}
+	else
+		error_dup_elem(d, line);
+}
+
+/* void populate_map(t_data *d, int lines_read, char *file_name)
+{
+    char *line;
+	int i;
+
+	line = NULL;
+	i = 0;
+    d->map_utils->map = malloc(sizeof(char *) * (lines_read + 1));
+    line = read_map_lines2(d, file_name);
+    while (line)
+	{
+        process_map_line(d, line, i);
+        free(line);
+        line = read_map_lines2(d, file_name);
+        i++;
+    }
+    d->map_utils->map[i] = "\0";
+} */
+
+
+void populate_map(t_data *d, int lines_read, char *file_name)
+{
+    char *line;
+	
+    d->line_height = lines_read;
+    d->map_utils->map = malloc(sizeof(char *) * (lines_read + 1));
+    close(d->fd);
+    d->fd = open(file_name, O_RDONLY);
+    skip_lines(d, d->map_utils->skip_count);
+    line = get_next_line(d->fd);
+	d->map_utils->skip_count = 0;
+    while (line)
+	{
+        if (!line)
+            break;
+		if (check_enter(line))
+        	;
+		else
+		{
+			process_map_line(d, line);
+			d->map_utils->skip_count++;
+		}
+		free(line);
+        line = get_next_line(d->fd);
+    }
+    d->map_utils->map[d->map_utils->skip_count] = "\0";
+}
+
+void map_validation_test(t_data *d, char *file_name)
+{
+    int lines_read;
+	int i;
+
+	lines_read = 0;
+    i = d->map_utils->skip_count;
+    d->fd = open(file_name, O_RDONLY);
+    skip_lines(d, i);
+    read_map_lines(d, &lines_read);
+    populate_map(d, lines_read, file_name);
+    if (is_map_closed2(d))
+        printf("Mapa FECHADO!\n");
+    else
+        printf("Mapa invalido!\n");
+    d->map_utils->map[lines_read] = NULL;
+    close(d->fd);
+}
+
+
+/* void	map_validation_test(t_data *d, char *file_name)
 {
     char *line;
 	int i;
@@ -267,11 +415,6 @@ void	map_validation_test(t_data *d, char *file_name)
 		j = 0;
 		free(line);
 		line = get_next_line(d->fd);
-		/* if (!ft_strcmp(line, "\t"))//Se existir linhas com tab ignora a linha!
-		{
-			free(line);
-			line = get_next_line(d->fd);
-		} */
 		if(!line)
 			break;
 		if(check_enter(line))
@@ -284,12 +427,8 @@ void	map_validation_test(t_data *d, char *file_name)
 				while(line[j] != '\0' && line[j] != '\n')
 				{
 					d->map_utils->map[i][j] = line[j];
-					//printf("map*%s", d->map_utils->map[i]);
 					j++;		
 				}
-				/* if(line[j] == '\n')
-					d->map_utils->map[i][j] = ' ';
-				j++; */
 				while(j < d->line_length)
 				{
 					d->map_utils->map[i][j] = ' ';
@@ -302,7 +441,6 @@ void	map_validation_test(t_data *d, char *file_name)
 		}
 		else
 			error_dup_elem(d, line);
-		//printf("%s", line);
 	}
 	d->map_utils->map[i] = "\0";
 	if(is_map_closed2(d))
@@ -311,4 +449,4 @@ void	map_validation_test(t_data *d, char *file_name)
 		printf("Mapa invalido!\n");
 	d->map_utils->map[i] = NULL;
 	close(d->fd);
-}
+} */
